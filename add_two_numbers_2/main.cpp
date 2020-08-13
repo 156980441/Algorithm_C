18eb7578cd139e1d442add03844778149913de60
fanyl <yunlong.fan@okcoin.com>
2020-08-13 18:10:32 +0800

    1|【提交类型】new 【修改内容】leetCode 445 two numbers add 【影响范围】bb 【风险评估】low

---
+++ add_two_numbers_2/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct ListNode
+{
+    int val;
+    struct ListNode *next;
+};
+
+int main()
+{
+    printf("hello\n");
+    printf("world");
+    return 1;
+}
+
+struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2)
+{
+}
