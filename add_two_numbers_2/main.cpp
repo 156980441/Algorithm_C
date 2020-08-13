40e002ae8a04523b54a7036558d4c6762f3227bc
fanyl <yunlong.fan@okcoin.com>
2020-08-13 18:54:27 +0800

    1|【提交类型】new 【修改内容】leetCode 445 two numbers add 【影响范围】bb 【风险评估】low

--- add_two_numbers_2/main.cpp
+++ add_two_numbers_2/main.cpp
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct ListNode
 {
     int val;
     struct ListNode *next;
 };
+
+#define ListNode struct ListNode
+
+int ListLength(ListNode *head);
 
 int main()
 {
@@ -13,7 +18,29 @@ int main()
     return 1;
 }
 
-struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2)
+ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
 {
+    int sizeOfFirst = ListLength(l1);
+    int sizeOfSecond = ListLength(l2);
     return NULL;
+}
+
+ListNode *initList (ListNode **head) 
+{
+    *head = (ListNode *)malloc(sizeof(ListNode));
+    (*head)->next = NULL;
+}
+
+// 求长度
+int ListLength(ListNode *head)
+{
+    ListNode *p = head;
+    int size = 0;
+    while (p->next != NULL)
+    {
+        p = p->next;
+        size ++;
+    }
+    
+    return size;
 }
