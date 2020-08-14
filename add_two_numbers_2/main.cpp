603c14ba541013d19a98cc20cd7b84a45a85a96d
fanyl <yunlong.fan@okcoin.com>
2020-08-14 16:18:00 +0800

    1|【提交类型】new 【修改内容】leetCode 445 two numbers add 【影响范围】bb 【风险评估】low

--- add_two_numbers_2/main.cpp
+++ add_two_numbers_2/main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 struct ListNode
 {
@@ -10,6 +11,9 @@ struct ListNode
 #define ListNode struct ListNode
 
 int ListLength(ListNode *head);
+void initList(ListNode **head);
+int ListInsert(ListNode *head, int index, int data);
+void reverseList(ListNode *head, ListNode **outputHead);
 
 int main()
 {
@@ -22,13 +26,70 @@ ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
 {
     int sizeOfFirst = ListLength(l1);
     int sizeOfSecond = ListLength(l2);
-    return NULL;
+    ListNode *firstLast = l1 + sizeOfFirst;
+    ListNode *secondLast = l2 + sizeOfSecond;
+    int max = sizeOfFirst > sizeOfSecond ? sizeOfFirst : sizeOfSecond;
+    ListNode **newHead;
+    initList(newHead);
+    bool carry = 0;
+    for (int i = 0; i < max; i++)
+    {
+        int val = firstLast->val + secondLast->val;
+        if (carry == 1) // 上一个存在进位
+        {
+            val = val + 1;
+            carry = 0;
+        }
+
+        if (val > 10)
+        {
+            carry = 1;
+            ListInsert(*newHead, i, val - 10);
+        }
+        else
+        {
+            ListInsert(*newHead, i, val);
+        }
+    }
+
+    ListNode **outputHead;
+    reverseList(*newHead, outputHead);
+    return *outputHead;
 }
 
-ListNode *initList (ListNode **head) 
+
+void reverseList(ListNode *head, ListNode **outputHead)
+{
+    initList(outputHead);
+    int size = ListLength(head);
+    ListNode *p = head + size - 1;
+    int index = 0;
+    while (p >= head)
+    {
+        ListInsert(*outputHead, 0, p->val);
+        index++;
+    }
+}
+
+// 初始化
+void initList(ListNode **head)
 {
     *head = (ListNode *)malloc(sizeof(ListNode));
     (*head)->next = NULL;
+}
+
+// 销毁
+void destroy(ListNode **head)
+{
+    ListNode *p, *p1;
+    p = *head;
+    while (p != NULL)
+    {
+        p1 = p;
+        p = p->next;
+        free(p1);
+    }
+    *head = NULL;
 }
 
 // 求长度
@@ -39,8 +100,47 @@ int ListLength(ListNode *head)
     while (p->next != NULL)
     {
         p = p->next;
-        size ++;
+        size++;
     }
-    
+
     return size;
+}
+
+// 插入
+int ListInsert(ListNode *head, int index, int data)
+{
+    ListNode *p, *q;
+    p = head;
+    int j = -1;
+    while (p->next != NULL && j < index - 1)
+    {
+        p = p->next;
+        j++;
+    }
+    if (j != index - 1)
+        return 0;
+
+    q = (ListNode *)malloc(sizeof(ListNode));
+    q->val = data;
+    q->next = p->next;
+    p->next = q;
+    return 1;
+}
+
+// 取数据元素
+int ListGet(ListNode *head, int index, int *data)
+{
+    ListNode *p;
+    int j;
+    p = head;
+    while (p->next != NULL)
+    {
+        if (p == head + index)
+        {
+            *data = p->val;
+            return 1;
+        }
+        p++;
+    }
+    return 0;
 }
